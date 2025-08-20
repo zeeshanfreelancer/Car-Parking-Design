4d3b8358bea725971df6c5574df0cfdbc590f529
Zeeshan <zeeshankhan469224@gmail.com>
2025-08-20 14:59:01 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+function App() {
+
+
+  return (
+    <div className = 'flex items-center justify-center h-screen bg-green-100'>
+      <div className='bg-white p-6 rounded-2xl shadow-lg text-center'>
+            <h1 className='text-3xl font-bold text-blue-600'>Welcome in the world of programming</h1>
+            <p className='mt-4 text-gray-600'>Let's start programming</p>
+            <button className='mt-6 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg'>
+              Click Me
+            </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
