59ce3b123a89fc526b2c3286e869a7101250985f
Zeeshan <zeeshankhan469224@gmail.com>
2025-08-21 17:21:30 +0500

    remove from app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Dashboard from './components/Dashboard/Dashboard'
 import Header from './components/Header'
 
+
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header/>
       <Dashboard />
+      
 
     </>
   )
